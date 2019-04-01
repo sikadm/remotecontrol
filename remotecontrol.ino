ab69953f36bcb960ec16dd0205613e7b343a9de8
sikadm <33527388+sikadm@users.noreply.github.com>
2019-03-31 23:09:40 -0400

    Add files via upload

---
+++ remotecontrol.ino
@@ -0,0 +1,20 @@
+/* This is just some pseudo-code till the components are 
+ *  determined and the innerworkings can be decided
+ */
+
+//include appropriate libraries
+// assign pins variable names
+
+void setup() {
+
+}
+
+void loop() {
+//if button pressed
+  //while photodiode low --> receiver has not said it received
+    //send signal using protocol 
+/*once photodiode confirms the message has been received,
+ * the IR LED will stop transmitting the message
+ */
+ 
+}
