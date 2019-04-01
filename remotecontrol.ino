a099f05b4e6f6d17c13d1332c62d9614ce607348
sikadm <33527388+sikadm@users.noreply.github.com>
2019-04-01 10:40:01 -0400

    Add files via upload

--- remotecontrol.ino
+++ remotecontrol.ino
@@ -1,18 +1,78 @@
-/* This is just some pseudo-code till the components are 
- *  determined and the innerworkings can be decided
- */
 
 //include appropriate libraries
 // assign pins variable names
+#define BUTTON 1
+#define IR_LED A5
+#define PHOTODIODE 7
 
 void setup() {
-
+  pinMode(BUTTON, INPUT);
+  pinMode(IR_LED, OUTPUT);
+  pinMode(PHOTODIODE, INPUT);
 }
 
 void loop() {
 //if button pressed
+  if (BUTTON == HIGH){
   //while photodiode low --> receiver has not said it received
-    //send signal using protocol 
+    while (PHOTODIODE == LOW){
+    //send signal using protocol-- RC-5 power 
+      digitalWrite(IR_LED, HIGH);
+      delay(2.4);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(1.2);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(1.2);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(1.2);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(1.2);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(0.6);
+      digitalWrite(IR_LED, HIGH);
+      delay(0.6);
+      digitalWrite(IR_LED, LOW);
+      delay(270);
+    }
+  }
+
+
 /*once photodiode confirms the message has been received,
  * the IR LED will stop transmitting the message
  */
